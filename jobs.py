51553648fa64c34178cbf91bdce0cdeb4d9d1aef
Speedy205 <155118834+Speedy205@users.noreply.github.com>
2026-01-31 17:31:41 +0100

    Add files via upload

---
+++ jobs.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from commands import check_alerts_job, daily_check_job, morning_meteo_job, tomorrow_rain_job
+
+__all__ = [
+    "check_alerts_job",
+    "daily_check_job",
+    "tomorrow_rain_job",
+    "morning_meteo_job",
+]
